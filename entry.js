778b67d481bf5337a9235a986fd3447c30300067
Nagarjuna Kuppala <nagarjunakuppala07@gmail.com>
2018-06-09 13:59:18 -0500

    committed by nagarjuna

--- entry.js
+++ entry.js
@@ -2,7 +2,7 @@ var express = require('express');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var cors = require('cors');
-// Test
+// Test by nagarjuna
 var app = express();
 
 const route = require('./route/routes');
